d0648d35d854eedee681f44023c63c6b2cbf39fe
Gene Harvey <gharveymn@gmail.com>
2018-10-07 15:11:07 -0500

    fix formatting

--- validatestring.cc
+++ validatestring.cc
@@ -20,11 +20,10 @@ along with Octave; see the file COPYING.  If not, see
 
 */
 
-#include <octave/oct.h>
 #include <octave/oct-string.h>
+#include <octave/oct.h>
 
-DEFUN_DLD (validatestring, args, nargout, 
-"-*- texinfo -*-\n\
+DEFUN_DLD (validatestring, args, nargout, "-*- texinfo -*-\n\
 @deftypefn  {} {@var{validstr} =} validatestring (@var{str}, @var{strarray})\n\
 @deftypefnx {} {@var{validstr} =} validatestring (@var{str}, @var{strarray}, @var{funcname})\n\
 @deftypefnx {} {@var{validstr} =} validatestring (@var{str}, @var{strarray}, @var{funcname}, @var{varname})\n\
@@ -64,167 +63,184 @@ validatestring (\"b\", @{\"red\", \"green\", \"blue\", \"black\"@})\n\
 @seealso{strcmp, strcmpi, validateattributes, inputParser}\n\
 @end deftypefn ")
 {
-  octave_idx_type          i;
-  std::string              errstr;
-  std::string              non_match_str;
-  
-  octave_value             ov_str;
-  octave_value             ov_strarray;
-  octave_value             ov_funcname;
-  octave_value             ov_varname;
-  
-  std::string        str;
+  octave_idx_type i;
+  std::string errstr;
+  std::string non_match_str;
+
+  octave_value ov_str;
+  octave_value ov_strarray;
+  octave_value ov_funcname;
+  octave_value ov_varname;
+
+  std::string str;
   Array<std::string> strarray;
-  std::string        funcname;
-  std::string        varname;
-  
-  octave_idx_type          num_strs, nmatches;
-  Array<octave_idx_type>   matches;
-  
-  octave_idx_type          len, min_len, min_len_idx;
-  
-  octave_idx_type          nstr     = 0;
-  octave_idx_type          nargin   = args.length ();
-  octave_idx_type          position = 0;
-  
+  std::string funcname;
+  std::string varname;
+
+  octave_idx_type num_strs, nmatches;
+  Array<octave_idx_type> matches;
+
+  octave_idx_type len, min_len, min_len_idx;
+
+  octave_idx_type nstr = 0;
+  octave_idx_type nargin = args.length ();
+  octave_idx_type position = 0;
+
   if (nargin < 2 || nargin > 5)
-  {
-    print_usage ();
-    return octave_value ();
-  }
-  
+    {
+      print_usage ();
+      return octave_value ();
+    }
+
   ov_str      = args(0);
   ov_strarray = args(1);
-  
+
   for (i = 2; i < nargin; i++)
-  {
-    if (args(i).is_string ())
     {
-      switch (nstr)
-      {
-        case 0:  ov_funcname = args(i); nstr++; break;
-        case 1:  ov_varname  = args(i); nstr++; break;
-        default: error ("validatestring: invalid number of character inputs (3)");
-      }
+      if (args (i).is_string ())
+        {
+          switch (nstr)
+            {
+            case 0:
+              ov_funcname = args(i);
+              nstr++;
+              break;
+            case 1:
+              ov_varname = args(i);
+              nstr++;
+              break;
+            default:
+              error ("validatestring: invalid number of character inputs (3)");
+            }
+        }
     }
-  }
-  
+
   if (nstr > 2)
-  {
-    error (("validatestring: invalid number of character inputs (" + std::to_string(nstr) + ")").c_str ());
-  }
-  
-  if (nargin > 2 && args(nargin - 1).isnumeric ())
-  {
-    position = (args(nargin - 1).fix ()).idx_type_value ();
-  }
-  
-  if (! ov_str.is_string ())
-  {
-    error ("validatestring: STR must be a character string");
-  }
+    {
+      error (("validatestring: invalid number of character inputs ("
+              + std::to_string (nstr) + ")")
+                 .c_str ());
+    }
+
+  if (nargin > 2 && args (nargin - 1).isnumeric ())
+    {
+      position = (args (nargin - 1).fix ()).idx_type_value ();
+    }
+
+  if (!ov_str.is_string ())
+    {
+      error ("validatestring: STR must be a character string");
+    }
   else if (ov_str.ndims () != 2 || ov_str.dims ()(0) != 1)
-  {
-    error ("validatestring: STR must be a single row vector");
-  }
+    {
+      error ("validatestring: STR must be a single row vector");
+    }
   else if (ov_strarray.isempty ())
-  {
-    error ("validatestring: STRARRAY must be non-empty");
-  }
-  else if (! ov_strarray.iscellstr ())
-  {
-    error ("validatestring: STRARRAY must be a cellstr");
-  }
-  else if (! ov_funcname.isempty () && (ov_funcname.ndims () != 2 || ov_funcname.dims ()(0) != 1))
-  {
-    error ("validatestring: FUNCNAME must be a single row vector");
-  }
-  else if (! ov_varname.isempty () && (ov_varname.ndims () != 2 || ov_varname.dims ()(0) != 1))
-  {
-    error ("validatestring: VARNAME must be a single row vector");
-  }
+    {
+      error ("validatestring: STRARRAY must be non-empty");
+    }
+  else if (!ov_strarray.iscellstr ())
+    {
+      error ("validatestring: STRARRAY must be a cellstr");
+    }
+  else if (!ov_funcname.isempty ()
+           && (ov_funcname.ndims () != 2 || ov_funcname.dims ()(0) != 1))
+    {
+      error ("validatestring: FUNCNAME must be a single row vector");
+    }
+  else if (!ov_varname.isempty ()
+           && (ov_varname.ndims () != 2 || ov_varname.dims ()(0) != 1))
+    {
+      error ("validatestring: VARNAME must be a single row vector");
+    }
   else if (position < 0)
-  {
-     error ("validatestring: POSITION must be >= 0");
-  }
-  
-  str      = ov_str.string_value ();
+    {
+      error ("validatestring: POSITION must be >= 0");
+    }
+
+  str = ov_str.string_value ();
   strarray = ov_strarray.cellstr_value ();
-  
-  if (! ov_funcname.isempty ())
-  {
-    funcname = ov_funcname.string_value ();
-    errstr   = funcname + ": ";
-  }
-  
-  if (! ov_varname.isempty ())
-  {
-    varname =  ov_varname.string_value ();
-    errstr  += varname + " ";
-  }
+
+  if (!ov_funcname.isempty ())
+    {
+      funcname = ov_funcname.string_value ();
+      errstr = funcname + ": ";
+    }
+
+  if (!ov_varname.isempty ())
+    {
+      varname = ov_varname.string_value ();
+      errstr += varname + " ";
+    }
   else
-  {
-    errstr += "'" + str + "' ";
-  }
-  
+    {
+      errstr += "'" + str + "' ";
+    }
+
   if (position > 0)
-  {
-    errstr += "(argument #" + std::to_string(position) + ") ";
-  }
-  
+    {
+      errstr += "(argument #" + std::to_string (position) + ") ";
+    }
+
   num_strs = strarray.numel ();
-  matches  = Array<octave_idx_type>(dim_vector(num_strs,1));
+  matches = Array<octave_idx_type> (dim_vector (num_strs, 1));
   nmatches = 0;
   for (i = 0; i < num_strs; i++)
-  {
-    if (octave::string::strncmpi (str, strarray(i), str.length ()) != 0)
     {
-      matches(nmatches) = i;
-      nmatches++;
-    }
-  }
-  
-  if (nmatches == 0)
-  {
-    non_match_str = strarray(0);
-    for (i = 1; i < num_strs; i++)
-    {
-      non_match_str += ", " + strarray(i);
-    }
-    error (("validatestring: " + errstr + "does not match any of\n" + non_match_str).c_str ());
-  }
-  else if (nmatches == 1)
-  {
-    return octave_value (strarray(matches(0)));
-  }
-  else
-  {
-    min_len     = strarray(matches(0)).length ();
-    min_len_idx = 0;
-    for (i = 1; i < nmatches; i++)
-    {
-      len = strarray(matches(i)).length ();
-      if (len < min_len)
-      {
-        min_len = len;
-        min_len_idx = i;
-      }
-    }
-    
-    for (i = 0; i < nmatches; i++)
-    {
-      if (i != min_len_idx && ! octave::string::strncmpi (strarray(matches(min_len_idx)), strarray(matches(i)), min_len))
-      {
-        non_match_str = strarray(matches(0));
-        for (i = 1; i < nmatches; i++)
+      if (octave::string::strncmpi (str, strarray(i), str.length ()) != 0)
         {
-          non_match_str += ", " + strarray(matches(i));
+          matches (nmatches) = i;
+          nmatches++;
         }
-        error (("validatestring: " + errstr + "allows multiple unique matches:\n" + non_match_str).c_str ());
-      }
     }
-    return octave_value (strarray(matches(min_len_idx)));
-  }
+
+  if (nmatches == 0)
+    {
+      non_match_str = strarray(0);
+      for (i = 1; i < num_strs; i++)
+        {
+          non_match_str += ", " + strarray(i);
+        }
+      error (("validatestring: " + errstr + "does not match any of\n"
+              + non_match_str)
+                 .c_str ());
+    }
+  else if (nmatches == 1)
+    {
+      return octave_value (strarray(matches(0)));
+    }
+  else
+    {
+      min_len = strarray(matches(0)).length ();
+      min_len_idx = 0;
+      for (i = 1; i < nmatches; i++)
+        {
+          len = strarray(matches(i)).length ();
+          if (len < min_len)
+            {
+              min_len = len;
+              min_len_idx = i;
+            }
+        }
+
+      for (i = 0; i < nmatches; i++)
+        {
+          if (i != min_len_idx
+              && !octave::string::strncmpi (strarray(matches(min_len_idx)),
+                                            strarray(matches(i)), min_len))
+            {
+              non_match_str = strarray (matches (0));
+              for (i = 1; i < nmatches; i++)
+                {
+                  non_match_str += ", " + strarray (matches (i));
+                }
+              error (("validatestring: " + errstr
+                      + "allows multiple unique matches:\n" + non_match_str)
+                         .c_str ());
+            }
+        }
+      return octave_value (strarray(matches(min_len_idx)));
+    }
 }
 
 /*
@@ -237,19 +253,27 @@ validatestring (\"b\", @{\"red\", \"green\", \"blue\", \"black\"@})\n\
 %!assert (validatestring ("d", strarray), "def")
 
 %!error <'xyz' does not match any> validatestring ("xyz", strarray)
-%!error <DUMMY_TEST: 'xyz' does not> validatestring ("xyz", strarray, "DUMMY_TEST")
-%!error <DUMMY_TEST: DUMMY_VAR does> validatestring ("xyz", strarray, "DUMMY_TEST", "DUMMY_VAR")
-%!error <DUMMY_TEST: DUMMY_VAR \(argument #5\) does> validatestring ("xyz", strarray, "DUMMY_TEST", "DUMMY_VAR", 5)
+%!error <DUMMY_TEST: 'xyz' does not> validatestring ("xyz", strarray,
+"DUMMY_TEST")
+%!error <DUMMY_TEST: DUMMY_VAR does> validatestring ("xyz", strarray,
+"DUMMY_TEST", "DUMMY_VAR")
+%!error <DUMMY_TEST: DUMMY_VAR \(argument #5\) does> validatestring ("xyz",
+strarray, "DUMMY_TEST", "DUMMY_VAR", 5)
 %!error <'abc' allows multiple unique matches> validatestring ("abc", strarray)
 
 ## Test input validation
 %!error validatestring ("xyz")
 %!error validatestring ("xyz", {"xyz"}, "3", "4", 5, 6)
-%!error <invalid number of character inputs> validatestring ("xyz", {"xyz"}, "3", "4", "5")
-%!error <STR must be a character string> validatestring (1, {"xyz"}, "3", "4", 5)
-%!error <STR must be a single row vector> validatestring ("xyz".', {"xyz"}, "3", "4", 5)
+%!error <invalid number of character inputs> validatestring ("xyz", {"xyz"},
+"3", "4", "5")
+%!error <STR must be a character string> validatestring (1, {"xyz"}, "3", "4",
+5)
+%!error <STR must be a single row vector> validatestring ("xyz".', {"xyz"},
+"3", "4", 5)
 %!error <STRARRAY must be a cellstr> validatestring ("xyz", "xyz", "3", "4", 5)
-%!error <FUNCNAME must be a single row vector> validatestring ("xyz", {"xyz"}, "33".', "4", 5)
-%!error <VARNAME must be a single row vector> validatestring ("xyz", {"xyz"}, "3", "44".', 5)
+%!error <FUNCNAME must be a single row vector> validatestring ("xyz", {"xyz"},
+"33".', "4", 5)
+%!error <VARNAME must be a single row vector> validatestring ("xyz", {"xyz"},
+"3", "44".', 5)
 %!error <POSITION must be> validatestring ("xyz", {"xyz"}, "3", "4", -5)
 */
